18b7d0d9cba8f195ed41ccbedb600821e7b06c3c
Giulio Eulisse <giulio.eulisse@cern.ch>
2014-05-20 10:12:53 +0200

    Apply fastjet-3.0.1-nodegeneracywarning.patch

--- src/Voronoi.cc
+++ src/Voronoi.cc
@@ -117,7 +117,7 @@ using namespace std;
 
 FASTJET_BEGIN_NAMESPACE
 
-LimitedWarning VoronoiDiagramGenerator::_warning_degeneracy;
+LimitedWarning VoronoiDiagramGenerator::_warning_degeneracy = 0;
 
 VoronoiDiagramGenerator::VoronoiDiagramGenerator(){
   siteidx = 0;
